e188b038dba54e14dd861f4f9c1a2665de957fa4
ChunHan08 <149086722+ChunHan08@users.noreply.github.com>
2024-08-05 16:27:19 +0000

    update

---
+++ word.js
@@ -0,0 +1,70 @@
+const wordList = [
+  {
+    word: "python",
+    hint: "programming language"
+  },
+  {
+    word: "piano",
+    hint: "musical instrument"
+  },
+  {
+    word: "head",
+    hint: "part of the body"
+  },
+  {
+    word: "poop",
+    hint: "smelly"
+  },
+  {
+  word: "hello",
+  hint: "greeting"
+  },
+  {
+  word: "pencil",
+  hint: "writing utensil"
+  },
+  {
+    word: "Ipad",
+    hint: "electronic device"
+  },
+  {
+    word: "socks",
+    hint: "clothing"
+  },
+  {
+    word: "chocolate",
+    hint: "sweet"
+  },
+  {
+    word: "chips",
+    hint: "snack"
+  },
+  {
+    word: "water",
+    hint: "liquid"
+  },
+  {
+    word: "ruler",
+    hint: "measuring tool"
+  },
+  {
+    word: "car",
+    hint: "automobile"
+  },
+  {
+    word: "earth",
+    hint: "planet"
+  },
+  {
+    word: "supernova",
+    hint: "explosion"
+  },
+  {
+    word: "qwerty",
+    hint: "keyboard"
+  },
+  {
+    word: "sigma",
+    hint: "greek letter"
+  },
+]
